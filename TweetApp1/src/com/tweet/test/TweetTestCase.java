f1e63f5d70f9afe1bcdc5d61253ec7cc9b1781fa
vivekjais7460 <85106353+vivekjais7460@users.noreply.github.com>
2022-11-10 22:21:46 +0530

    First Commit

---
+++ TweetApp1/src/com/tweet/test/TweetTestCase.java
@@ -0,0 +1,14 @@
+package com.tweet.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TweetTestCase {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
